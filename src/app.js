3f8d4350bb264f981aab3e47879ba88c98675a7b
tonyg <aj.gianno@gmail.com>
2021-01-14 17:45:18 -0600

    refactored i.e. modularization

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const ArticlesService = require('./articles-service');
-const knex = require('knex');
+const articlesRouter = require('./articles/articles-router');
 
 const { NODE_ENV } = require('./config');
 
@@ -19,30 +18,11 @@ app.use(express.json());
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 
-//Routes
-app.get('/', (req, res, next) => {
-  JSON.parse('{"key" : "value"}');
-
+app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
 
-app.get('/articles', (req, res, next) => {
-  ArticlesService.getAllArticles(req.app.get('db'))
-    .then((articles) => {
-      res.json(articles);
-    })
-    .catch(next);
-});
-
-app.post('/articles', (req, res, next) => {
-  const { title, content, style } = req.body;
-  const newArticle = { title, content, style };
-  ArticlesService.insertArticle(req.app.get('db'), newArticle).then(
-    (article) => {
-      res.status(201).location(`/articles/${article.id}`).json(article);
-    }
-  );
-});
+app.use('/articles', articlesRouter);
 
 // Error handlers
 app.use(function errorHandler(error, req, res, next) {
