d7cc2a2b85828b8f910f34b5ac3f868552639062
tonyg <aj.gianno@gmail.com>
2021-01-15 12:25:34 -0600

    sanitize via xss

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,25 +1,30 @@
 const express = require('express');
+const xss = require('xss');
 
 const ArticlesService = require('./articles-service');
 const articlesRouter = express.Router();
 
-// articlesRouter.get('/', (req, res, next) => {
-//   JSON.parse('{"key" : "value"}');
-
-//   res.json({ message: 'Hello, world!' });
-// });
-
 articlesRouter.get('/', (req, res, next) => {
   ArticlesService.getAllArticles(req.app.get('db'))
     .then((articles) => {
-      res.json(articles);
+      const sanitizeArticles = articles.map((article) => {
+        return {
+          article,
+          title: xss(article.title),
+          content: xss(article.content),
+          style: xss(article.style),
+        };
+      });
+      res.json(sanitizeArticles);
     })
     .catch(next);
 });
 
 articlesRouter.post('/', (req, res, next) => {
   const { title, content, style } = req.body;
+
   const newArticle = { title, content, style };
+
   ArticlesService.insertArticle(req.app.get('db'), newArticle)
     .then((article) => {
       res.status(201).location(`/articles/${article.id}`).json(article);
